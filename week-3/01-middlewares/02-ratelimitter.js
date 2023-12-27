36fb17eeabcba65642ff440421d6fbc6eaf55ad1
Aayush Kubba <aayushkubba.727@gmail.com>
2023-12-28 03:11:28 +0530

    week-3 assignment solutions

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers['user-id'];
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] ++;
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).json({ msg: 'Too many requests' });
+    }
+    else {
+      next();
+    }
+  }
+  else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+});
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
